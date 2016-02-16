c86d63bb871947f4804b686ec66834637bd3ec52
Dmitriy Olshevskiy <olshevskiy87@bk.ru>
2016-02-16 20:28:37 +0200

    make an sql queries more readable

--- gitmyfeeds_bot.py
+++ gitmyfeeds_bot.py
@@ -27,11 +27,12 @@ conn = psycopg2.connect(
 cur = conn.cursor()
 # now use test user only
 user_id = 1
-cur.execute(
-    "select uat.token, gu.username "
-    "from users_atom_tokens uat "
-    "    join github_users gu on gu.user_id = uat.user_id "
-    "where uat.user_id = %s" % user_id)
+cur.execute("""
+    select uat.token, gu.username
+    from users_atom_tokens uat
+        join github_users gu on gu.user_id = uat.user_id
+    where uat.user_id = %s
+            """ % user_id)
 atom = cur.fetchone()
 
 # get feeds for one test user
@@ -74,12 +75,14 @@ for entry in soup.find_all('entry'):
     if quote is not None:
         entry_text = quote.get_text().strip()
 
-    cur.execute(
-        "insert into feeds_private("
-        "user_id, event, entry_id, published, title, author, content, link"
-        ") values (%s, %s, %s, %s, %s, %s, %s, %s) "
-        "on conflict (entry_id) do nothing",
-        (user_id, event, entry_id, published, title, author, entry_text, link))
+    cur.execute("""
+        insert into feeds_private(
+            user_id, event, entry_id, published, title, author, content, link
+        ) values (%s, %s, %s, %s, %s, %s, %s, %s)
+        on conflict (entry_id) do nothing
+                """, (user_id, event, entry_id, published,
+                      title, author, entry_text, link)
+                )
     conn.commit()
 
 # prepare telegram bot
@@ -87,19 +90,23 @@ bot = telegram.Bot(token=tg_bot['token'])
 
 # for all active chats send new feeds
 cur_feeds = conn.cursor()
-cur_feeds.execute(
-    "select fp.id, fp.title, fp.link, fp.content "
-    "    , to_char(fp.published, 'dd.mm.yy hh24:mi') dt "
-    "from feeds_private fp "
-    "    left join feeds_sent fs "
-    "        on fp.id = fs.feed_private_id and fs.user_id = %s "
-    "where fs.id is null "
-    "order by fp.published asc "
-    "limit %s "
-    % (user_id, tg_bot['send_feeds_limit']))
+cur_feeds.execute("""
+    select fp.id, fp.title, fp.link, fp.content
+        , to_char(fp.published, 'dd.mm.yy hh24:mi') dt
+    from feeds_private fp
+        left join feeds_sent fs
+            on fp.id = fs.feed_private_id and fs.user_id = %s
+    where fs.id is null
+    order by fp.published asc
+    limit %s
+                  """ % (user_id, tg_bot['send_feeds_limit']))
 
 cur_upd = conn.cursor()
-cur.execute("select chat_id from chats_to_send where active = true")
+cur.execute("""
+    select chat_id
+    from chats_to_send
+    where active = true
+            """)
 for chat in cur:
     for feed in cur_feeds:
         print 'send feed item [%s] to chat [%s]' \
@@ -117,10 +124,10 @@ for chat in cur:
             disable_web_page_preview=True)
 
         # mark as read to skip it next time
-        cur_upd.execute(
-            "insert into feeds_sent(feed_private_id, user_id) "
-            "values (%s, %s)"
-            % (feed['id'], user_id))
+        cur_upd.execute("""
+            insert into feeds_sent(feed_private_id, user_id)
+            values (%s, %s)
+                        """ % (feed['id'], user_id))
         conn.commit()
 
 cur_feeds.close()
